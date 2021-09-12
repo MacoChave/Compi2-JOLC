34981383eca45e695eb10941955c56d8b36b5d3b
MacoChave <201020831@ingenieria.usac.edu.gt>
2021-09-12 13:45:33 -0600

    GUI created

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask 
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Flask heroku app'
+    return render_template('index.html')
+
+@app.route('/editor', methods=['GET'])
+def editor():
+    return render_template('editor.html')
+
+@app.route('/analize', methods=['POST'])
+def analize():
+    return 'Analize page works!'
+
+@app.route('/reports')
+def reports():
+    return 'Reports page works!'
 
 if __name__ == '__main__':
     app.run()
